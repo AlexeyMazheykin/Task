1b4eea81a7aaabfa5354caeed2ac4741cb2fc56a
AlexeyMazheykin <65315231+AlexeyMazheykin@users.noreply.github.com>
2020-08-05 22:36:01 +0300

    gggg

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,10 @@
 jQuery(function() {
     $('.box__video').on('click', function () {
-        alert('q');
+        $('.modal').addClass('modal__active');
+    });
+    const modal = $('.modal').filter('modal__block');
+    $('.modal').not('modal__block').on('click', function () {
+        $(this).removeClass('modal__active');
         
     });
 });
